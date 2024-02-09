07e9fa93987b59baf7c5eab295abef7ca33d9686
Maya <mayawald3@gmail.com>
2024-02-09 14:22:54 -0600

    MW-1. Creating components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BehaviorSubject} from "rxjs";
+import {AsyncPipe} from "@angular/common";
+import {HomeComponent} from "./home/home.component";
+import {ProcessComponent} from "./process/process.component";
+import {RegisterComponent} from "./register/register.component";
+import {SignInComponent} from "./sign-in/sign-in.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, HomeComponent, ProcessComponent,
+    RegisterComponent, SignInComponent, WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'SmartBikeRackClient';
+  title = 'SmartBikeRackClient'
+  lockStatus$  = new BehaviorSubject('Locked')
+
+
+  onLockClicked() {
+    if (this.lockStatus$.value === 'Locked') {
+    this.lockStatus$.next('Unlocked')
+    } else {
+      this.lockStatus$.next('Locked')
+    }
+  }
 }
---
+++ src/app/process/process.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-process',
+  standalone: true,
+  imports: [],
+  templateUrl: './process.component.html',
+  styleUrl: './process.component.css'
+})
+export class ProcessComponent {
+
+}
