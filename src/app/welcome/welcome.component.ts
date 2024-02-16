a90bac35d267927c2d870ca6bf7e0759609b0de2
Maya <mayawald3@gmail.com>
2024-02-16 16:51:31 -0600

    MW-2. Welcome component and general working global styling

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
-
+  descriptionText = "BikeNest - Your solution for safely storing your beloved bicycle with just the " +
+    "touch of a button. Equipped with advanced locking mechanisms, BikeNest ensures that your bike stays safe " +
+    "and sound wherever you go. Say goodbye to fumbling with keys or locks and hello to peace of mind knowing " +
+    "that BikeNest will protect your bike for you. With BikeNest's secure webpage, you can see the status " +
+    "of your bike's lock at anytime. Register your bike today!"
 }
